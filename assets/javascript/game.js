f71dde54cb6982d8364d27017eb4c512752ef8e1
tricialeav <leavitt.tricia@gmail.com>
2018-02-10 17:59:56 -0800

    Created code to allow leia to be selected as avatar. First round against any oppnent functional.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,80 +1,93 @@
-// Player is asked to choose a character
-// on click, character photo is highlighted
-// player is asked to choose an enemy
-// on click, character and enemy move into place and other characters move to the side
-
-// both players start with full (100%) health
-// player clicks fight
-// character health and enemy health will be determined by a random number roll
-// Whichever avatar rolls highest inflicts damage
-// damage will also be determined by a random number roll and subtracted from loser health
-// if damage over certain %, crit
-
-// If player wins
-// Enemy player is dismissed and moves to defeated div
-// next character replaces
-// health set to 100%
-
-// If player loses
-// game over
+// Global Variables
 
 let leia;
 let luke;
 let vader;
 let boba;
+let avatar;
+let opponent;
 let p1health;
 let p2health;
-
-// Generates random number between 0 and 10
 let roll = Math.floor(Math.random() * 10);
 
+// Hide elements on start
+$(".characters").hide();
+$(".gameplay").hide();
+$(".attack").hide();
+$(".healthbar").hide();
+
 $(document).ready(function() {
-    $(".characters").hide();
-    $(".gameplay").hide();
-    $(".attack").hide();
-    $(".healthbar").hide();
+  avatar;
+  opponent;
+  leia = $("#leia");
+  luke = $("#luke");
+  vader = $("#vader");
+  boba = $("#boba");
+  p1health = $("#health1");
+  p2health = $("#health2");
+
+  //   Functions
+
+  function showHealth1() {
+    p1health.show("slow");
+  }
+  function showHealth2() {
+    p2health.show("slow");
+  }
+
+// Choose Avatar/Opponent
+
+// Leia
+
+  leia.on('click', function() {
+    avatar = "leia";
+    leia.animate({ top: "-137px" });
+    showHealth1();
+    luke.animate({ top: "95px" });
+    vader.animate({ top: "95px" });
+    boba.animate({ top: "95px" });
+    luke.off('click');
+    vader.off('click');
+    boba.off('click');
     
-    leia = $("#leia");
-    luke = $("#luke");
-    vader = $("#vader");
-    boba = $("#boba");
-    p1health = $("#health1");
-    p2health = $("#health2");
-
-  // character selection
-
-  leia.click(function() {
-    leia.animate({ top: '-137px' });
-    luke.animate({top: '95px', left: '-204px'});
-    vader.animate({top: '95px', left: '-204px'});
-    boba.animate({top: '95px', left: '-204px'});
+    // Opponent Luke
+    if( avatar == "leia"){
+        luke.click(function() {
+        luke.animate({ top: "350px", left: '-45px' });
+        showHealth2();
+        vader.animate({ top: "-25px", left: "400px" });
+        boba.animate({ top: "233px", left: "200px" });
+        $('.attack').show();
+      });
+    } 
   });
 
-  luke.click(function() {
-    leia.animate({ top: '95px' });
-    luke.animate({top: '-137px', left: '-204px'});
-    vader.animate({top: '95px', left: '-204px'});
-    boba.animate({top: '95px', left: '-204px'});
+  luke.on('click', function() {
+    avatar = "luke";
+    leia.animate({ top: "95px" });
+    luke.animate({ top: "-137px", left: "-204px" });
+    showHealth1();
+    vader.animate({ top: "95px", left: "-204px" });
+    boba.animate({ top: "95px", left: "-204px" });
   });
 
-  vader.click(function() {
-    leia.animate({ top: '95px' });
-    luke.animate({top: '95px' });
-    vader.animate({top: '-137px', left: '-408px'});
-    boba.animate({top: '95px', left: '-204px'});
+  vader.on('click', function() {
+    avatar = "vader";
+    leia.animate({ top: "95px" });
+    luke.animate({ top: "95px" });
+    vader.animate({ top: "-137px", left: "-408px" });
+    showHealth1();
+    boba.animate({ top: "95px", left: "-204px" });
   });
 
-  boba.click(function() {
-    leia.animate({ top: '95px' });
-    luke.animate({top: '95px' });
-    vader.animate({top: '95px' });
-    boba.animate({top: '-137px', left: '-612px'});
+  boba.on('click', function() {
+    avatar = "boba";
+    leia.animate({ top: "95px" });
+    luke.animate({ top: "95px" });
+    vader.animate({ top: "95px" });
+    boba.animate({ top: "-137px", left: "-612px" });
+    showHealth1();
   });
-
-
-
 
 
 });
-
-
