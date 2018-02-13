9030f9eeb5e58691249d4966ab01e6d0cd526602
tricialeav <leavitt.tricia@gmail.com>
2018-02-12 21:32:47 -0800

    minor adjustments and addition of Star Wars scrolling intro, base code found at CSS-Tricks

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -21,12 +21,85 @@ let curHp1;
 let curHp2;
 let gamesPlayed;
 
-// Hide elements on start
+// Make unused elements invisible @ start
 $("#avatar").css("visibility", "hidden");
 $("#opponents").css("visibility", "hidden");
 $(".attack").css("visibility", "hidden");
 $(".healthbar").css("visibility", "hidden");
 $("#h1No").css("visibility", "hidden");
+
+// Allow for scroll text
+function timeOut() {
+  $("#star").hide();
+  $("#wars").hide();
+  $("#gameTitle").hide();
+  dir.hide();
+  leia.hide();
+  luke.hide();
+  vader.hide();
+  boba.hide();
+}
+
+// Hide scroll text on refresh
+function hideIntro() {
+  $(".fade").hide();
+  $(".creditRoll").hide();
+  $(".crawl").hide();
+  $(".title").hide();
+  $(".scrollText").hide();
+}
+
+function clickToAttack() {
+  $(attack).removeAttr("style");
+  $(attack).text("Click to Attack");
+  attack.on("click", function() {
+    $(attack).show();
+  });
+}
+
+function stopAttack() {
+  attack.off("click");
+}
+
+// Retry on tie/loss
+function replay() {
+  hideIntro();
+  healthReset();
+  gamesPlayed = 0;
+  attackPower = Math.floor(Math.random() * (8 - 4 + 1) + 4);
+  attackPowerUp = attackPower;
+  $(".attack").css("visibility", "hidden");
+  dir.text("Please Select a Character");
+  avatar.removeAttr('style');
+  leia.removeAttr("style");
+  leia.css("visibility", "visible");
+  luke.removeAttr("style");
+  luke.css("visibility", "visible");
+  vader.removeAttr("style");
+  vader.css("visibility", "visible");
+  boba.removeAttr("style");
+  boba.css("visibility", "visible");
+  leia.on("click", function() {
+    stopAttack();
+    avatar = leia;
+    beginGame();
+  });
+  luke.on("click", function() {
+    stopAttack();
+    avatar = luke;
+    beginGame();
+  });
+  vader.on("click", function() {
+    stopAttack();
+    avatar = vader;
+    beginGame();
+  });
+  boba.on("click", function() {
+    stopAttack();
+    avatar = boba;
+    beginGame();
+  });
+}
 
 // General Functions
 
@@ -47,7 +120,7 @@ function healthReset() {
   curHp2 = hp2;
 }
 
-function clickOff() {
+function avatarClickOff() {
   leia.off("click");
   luke.off("click");
   vader.off("click");
@@ -55,48 +128,48 @@ function clickOff() {
 }
 
 function hideAll() {
-    leia.css("visibility", "hidden");
-    luke.css("visibility", "hidden");
-    vader.css("visibility", "hidden");
-    boba.css("visibility", "hidden");
-    avatarText.css("visibility", "hidden");
-    opponentText.css("visibility", "hidden");
-    p1HpBar.css("visibility", "hidden");
-    p2HpBar.css("visibility", "hidden");
-    p1HpText.css("visibility", "hidden");
-    p2HpText.css("visibility", "hidden");
-  }
-  
-  function hideOnWin() {
-    avatarText.css("visibility", "hidden");
-    opponentText.css("visibility", "hidden");
-    p1HpBar.css("visibility", "hidden");
-    p2HpBar.css("visibility", "hidden");
-    p1HpText.css("visibility", "hidden");
-    p2HpText.css("visibility", "hidden");
-  }
-  
-  function showOnRound() {
-    avatarText.css("visibility", "visible");
-    opponentText.css("visibility", "visible");
-    p1HpBar.css("visibility", "visible");
-    p2HpBar.css("visibility", "visible");
-    p1HpText.css("visibility", "visible");
-    p2HpText.css("visibility", "visible");
-  }
+  leia.css("visibility", "hidden");
+  luke.css("visibility", "hidden");
+  vader.css("visibility", "hidden");
+  boba.css("visibility", "hidden");
+  avatarText.css("visibility", "hidden");
+  opponentText.css("visibility", "hidden");
+  p1HpBar.css("visibility", "hidden");
+  p2HpBar.css("visibility", "hidden");
+  p1HpText.css("visibility", "hidden");
+  p2HpText.css("visibility", "hidden");
+}
+
+function hideOnWin() {
+  avatarText.css("visibility", "hidden");
+  opponentText.css("visibility", "hidden");
+  p1HpBar.css("visibility", "hidden");
+  p2HpBar.css("visibility", "hidden");
+  p1HpText.css("visibility", "hidden");
+  p2HpText.css("visibility", "hidden");
+}
+
+function showOnRound() {
+  avatarText.css("visibility", "visible");
+  opponentText.css("visibility", "visible");
+  p1HpBar.css("visibility", "visible");
+  p2HpBar.css("visibility", "visible");
+  p1HpText.css("visibility", "visible");
+  p2HpText.css("visibility", "visible");
+}
 
 //   Gameplay Functions
 
 function opponents() {
-  counterAttack = Math.floor(Math.random() * (20 - 10 + 1)) + 10;
+  counterAttack = Math.floor(Math.random() * (15 - 8 + 1)) + 8;
   dir.text("Click the Button to Attack");
   showHealth2();
-  $(".attack").css("visibility", "visible");
+  clickToAttack();
   avatarText.css("visibility", "visible");
   avatarText.text("Attack Multiplier: " + attackPowerUp);
   opponentText.css("visibility", "visible");
   opponentText.text("Counter Attack Multiplier: " + counterAttack);
-  clickOff();
+  avatarClickOff();
 }
 
 function leiaOpponent() {
@@ -184,21 +257,23 @@ function nextOpponent() {
 }
 
 function attacks() {
-  let p1Roll = Math.floor(Math.random() * 10);
-  let p2Roll = Math.floor(Math.random() * 10);
-  curHp1 = curHp1 - (p2Roll * counterAttack);
-  curHp2 = curHp2 - (p1Roll * attackPowerUp);
+  let p1Roll = Math.floor(Math.random() * 12);
+  let p2Roll = Math.floor(Math.random() * 12);
+  let p1TotalDMG = p1Roll * attackPowerUp;
+  let p2TotalDMG = p2Roll * counterAttack
+  curHp1 = curHp1 - (p2TotalDMG);
+  curHp2 = curHp2 - (p1TotalDMG);
   attackPowerUp = attackPower + attackPowerUp;
   avatarText.text("Attack Power: " + attackPowerUp);
+  dir.text('P1 attack: ' + p1TotalDMG + ' P2 attack: ' + p2TotalDMG);
   //   both player's hp above 1
   if (parseInt(curHp1) > 0 && parseInt(curHp2) > 0) {
     p1HpBar.val(curHp1);
     p1HpText.text(curHp1);
     p2HpBar.val(curHp2);
     p2HpText.text(curHp2);
-  } 
-  //   Player loses
-  else if (parseInt(curHp1) <= 0 && parseInt(curHp2) >= 1) {
+  } else if (parseInt(curHp1) <= 0 && parseInt(curHp2) >= 1) {
+    //   Player loses
     attack.off("click");
     hideAll();
     avatar.css("visibility", "hidden");
@@ -206,33 +281,31 @@ function attacks() {
       "You underestimate the power of the dark side. Press the button to play again."
     );
     attack.animate({ left: "0px" });
-    attack.text("Try Again?");
+    attack.text("Play Again?");
     attack.on("click", function() {
-      location.reload();
+      replay();
     });
   } else if (parseInt(curHp1) >= 0 && parseInt(curHp2) <= 0) {
     //   Player wins
     hideOnWin();
     gamesPlayed += 1;
-    if (gamesPlayed < 3 ) {
-    dir.text("The force is strong with you. Select next opponent.");
-    attack.off("click");
-    opponent.fadeTo("fast", 0);
-    attackPowerUp = attackPower;
-    nextOpponent();
-    } else{
-      dir.text("You've defeated your opponents! Remember – the Force will be with you, always.");
-      opponent.css('visibility', 'hidden');
-      attack.css('visibility', 'hidden');
-      if (avatar.attr('id') === leia.attr('id')) {
-        leia.animate({ top: '66px', left: '309px' });
-      } else if (avatar.attr('id') === luke.attr('id')) {
-        luke.animate({ top: '66px', left: '109px'});
-      } else if (avatar.attr('id') === vader.attr('id')) {
-        vader.animate({ top: '66px', left: '-103px'});
-      } else {
-        boba.animate({ top: '66px', left: '-303px' });
-      }
+    if (gamesPlayed < 3) {
+      dir.text("The force is strong with you. Select next opponent.");
+      attack.off("click");
+      opponent.fadeTo("fast", 0);
+      attackPowerUp = attackPower;
+      nextOpponent();
+    } else {
+      dir.text(
+        "You've defeated your opponents! Remember – the Force will be with you, always."
+      );
+      opponent.css("visibility", "hidden");
+      avatar.css("visibility", "hidden");
+      attack.animate({ left: "0px" });
+      attack.text("Play Again?");
+      attack.on("click", function() {
+        replay();
+      });
     }
   } else {
     //   Both die
@@ -241,14 +314,14 @@ function attacks() {
       "Never tell me the odds! Both players have died. Press the button to play again!"
     );
     attack.animate({ left: "0px" });
-    attack.text("Try Again?");
+    attack.text("Play Again?");
     attack.on("click", function() {
-      location.reload();
+      replay();
     });
   }
 }
 
-function stats() {
+function selectOpponentText() {
   dir.text("Please Select an Opponent");
   p1HpText.css("visibility", "visible");
   p1HpText.text(hp1);
@@ -259,26 +332,26 @@ function beginGame() {
   showHealth1();
   if (avatar.attr("id") === leia.attr("id")) {
     leia.animate({ top: "-280px" });
-    stats();
-    clickOff();
+    selectOpponentText();
+    avatarClickOff();
   } else if (avatar.attr("id") === luke.attr("id")) {
     luke.animate({ top: "-280px", left: "-204px" });
-    stats();
+    selectOpponentText();
     leia.animate({ left: "204px" });
-    clickOff();
+    avatarClickOff();
   } else if (avatar.attr("id") === vader.attr("id")) {
     vader.animate({ top: "-280px", left: "-408px" });
-    stats();
+    selectOpponentText();
     leia.animate({ left: "204px" });
     luke.animate({ left: "204px" });
-    clickOff();
+    avatarClickOff();
   } else if (avatar.attr("id") === boba.attr("id")) {
     boba.animate({ top: "-280px", left: "-612px" });
-    stats();
+    selectOpponentText();
     leia.animate({ left: "204px" });
     luke.animate({ left: "204px" });
     vader.animate({ left: "204px" });
-    clickOff();
+    avatarClickOff();
   }
 
   // Opponent Leia
@@ -302,6 +375,7 @@ function beginGame() {
 }
 
 function enableAttack() {
+  attack.css("visibility", "visible");
   attack.on("click", attacks);
 }
 
@@ -317,7 +391,7 @@ $(document).ready(function() {
   luke = $("#luke");
   vader = $("#vader");
   boba = $("#boba");
-  attackPower = Math.floor(Math.random() * 6) + 1;
+  attackPower = Math.floor(Math.random() * (8 - 4 + 1)) + 1;
   attackPowerUp = attackPower;
   p1HpBar = $("#health1");
   p2HpBar = $("#health2");
@@ -329,9 +403,22 @@ $(document).ready(function() {
   curHp2;
   gamesPlayed = 0;
 
-  dir.text("Please Select a Character");
+  timeOut();
 
-  // Choose Avatar to Begin
+  setTimeout(function() {
+    $("#star").fadeIn();
+    $("#wars").fadeIn();
+    $("#gameTitle").fadeIn();
+    dir.fadeIn();
+    leia.fadeIn();
+    luke.fadeIn();
+    vader.fadeIn();
+    boba.fadeIn();
+  }, 90000);
+
+  //  Choose Avatar to Begin
+
+  dir.text("Please Select a Character");
 
   leia.on("click", function() {
     avatar = leia;
